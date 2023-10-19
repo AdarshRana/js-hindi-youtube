3cdfe1336d82fc2396cb41ddbe1e965a27a6dad9
AdarshRana <111654893+AdarshRana@users.noreply.github.com>
2023-10-19 09:31:25 +0000

    conversions

---
+++ 01_basics/conversion_Operation.js
@@ -0,0 +1,33 @@
+let score="33"
+let score2=33
+let score3="33abc"
+console.log(typeof score)
+console.log(typeof score2)
+console.log(typeof score3)
+
+let sc=Number(score)
+let sc2=Number(score2)
+let sc3=Number(score3)
+
+console.log(typeof sc)
+console.log(typeof sc2)
+console.log(typeof sc3)
+
+console.log(sc)
+console.log(sc2)
+console.log(sc3)
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false=> 0;
+
+let ab=1
+let boolab=Boolean(ab);
+console.log(boolab);
+
+//1 =>true; 0=>false
+//"" => false;
+//"Adarsh" => true;
+
+let stab=String(ab)
+console.log(typeof(stab))
