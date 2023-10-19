1744dff310e687706e6711b78c70af477403451e
AdarshRana <111654893+AdarshRana@users.noreply.github.com>
2023-10-19 07:46:55 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=1445
+let accountEmail="adarsh@google.com"
+var accountPassword="12345"
+accountCity="Gurgaon";
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+//var has scope problem ,let has no scope problem
+
+let accountState;
+console.log(accountState)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+// alert("Hello") //we are using node js and not browser
+let age=22
+console.log(3+3)
+console.log("Hitesh")
+console.log(typeof "Adarsh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
