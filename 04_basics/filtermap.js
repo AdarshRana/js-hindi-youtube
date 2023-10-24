5ef08f53c94a9f35df4eac9b87731a95498c1517
AdarshRana <111654893+AdarshRana@users.noreply.github.com>
2023-10-24 12:36:24 +0000

    pp

--- 04_basics/filtermap.js
+++ 04_basics/filtermap.js
@@ -7,3 +7,10 @@ const values=coding.forEach((item)=>{
 const myNums=[1,2,3,4,5,6,7]
 const newNums=myNums.filter( (num) => num>4)   //if you use {} braces ,you need to return something 
 console.log(newNums);
+
+//const userBook=books.filter( (bk) => { return bk.publish >=2000}) //if you use {} then return is used
+//console.log(userBook);
+
+const Numss=[1,2,3,4,5,6,7,8,9,10]
+const n=Numss.map( (num) => num+10)
+console.log(n);
